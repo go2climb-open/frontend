0435d316124f82dc4b4ea2600b2ebd27641758fe
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2023-04-27 19:04:58 -0500

    refactoring

--- go2climb/src/app/app.module.ts
+++ go2climb/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
