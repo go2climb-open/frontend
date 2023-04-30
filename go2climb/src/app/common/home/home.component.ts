2d659e1d209694d4564865d01d0bb20b9f13fd28
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2023-04-30 16:32:20 -0500

    fixed styles

--- go2climb/src/app/common/home/home.component.ts
+++ go2climb/src/app/common/home/home.component.ts
@@ -9,7 +9,9 @@ export class HomeComponent implements  OnInit {
 
   services: any[] = [1,2,3,4,5,6];
   value = 'Clear me';
+  userType: boolean = true;
   constructor() { }
+
 
   ngOnInit() {
   }
