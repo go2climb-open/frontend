8d2074732cbd6611a964a9b80655a14b4b058b2c
Leonardo Aquino <u20201b949@upc.edu.pe>
2023-05-04 22:07:49 -0500

    Update register

--- go2climb/src/app/app.module.ts
+++ go2climb/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { SignUpComponent } from './security/pages/sign-up/sign-up.component';
-import { MatSelectModule } from '@angular/material/select'
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
