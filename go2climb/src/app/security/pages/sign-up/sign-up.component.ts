d381ad67e1fee2d0a77050500c8de9dc16252b94
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2023-06-04 17:25:35 -0500

    Update sign-up.component.ts

--- go2climb/src/app/security/pages/sign-up/sign-up.component.ts
+++ go2climb/src/app/security/pages/sign-up/sign-up.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {AuthService} from "../../service/auth.service";
 import {Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
+
 
 interface Type {
   value: string;
