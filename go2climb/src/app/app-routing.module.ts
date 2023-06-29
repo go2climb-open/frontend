75140d59fcb6118838d26cb6450c0d9fb9cdd26d
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2023-06-28 23:04:17 -0500

    feat: tourist account integration

--- go2climb/src/app/app-routing.module.ts
+++ go2climb/src/app/app-routing.module.ts
@@ -8,34 +8,32 @@ import { AddServiceComponent } from 'src/app/agency/agency-service/pages/add-ser
 import { ServiceDetailsComponent } from 'src/app/agency/agency-service/pages/service-details/service-details.component';
 import { ChangePasswordComponent } from './agency/change-password/change-password.component';
 import { UpgradePlansComponent } from './agency/upgrade-plans/upgrade-plans.component';
-import {SignUpTouristComponent} from "./security/pages/sign-up/tourist/sign-up-tourist/sign-up-tourist.component";
-import {SignUpAgencyComponent} from "./security/pages/sign-up/agency/sign-up-agency/sign-up-agency.component";
-import {ForgotPasswordComponent} from "./security/pages/forgot-password/forgot-password.component";
+import { SignUpTouristComponent } from './security/pages/sign-up/tourist/sign-up-tourist/sign-up-tourist.component';
+import { SignUpAgencyComponent } from './security/pages/sign-up/agency/sign-up-agency/sign-up-agency.component';
+import { ForgotPasswordComponent } from './security/pages/forgot-password/forgot-password.component';
 import { SearchComponent } from './common/search/search.component';
 import { PaymentDetailsComponent } from './agency/payment-details/payment-details.component';
 import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
-import {RequestAgencyComponent} from "./agency/request-agency/request-agency.component";
+import { RequestAgencyComponent } from './agency/request-agency/request-agency.component';
 
 const routes: Routes = [
-
   { path: 'sign-in', component: SignInComponent },
   { path: '', component: SignInComponent },
-  { path: 'sign-up-tourist',  component: SignUpTouristComponent},
-  { path: 'sign-up-agency',  component: SignUpAgencyComponent},
-  { path: 'forgot-password', component: ForgotPasswordComponent},
+  { path: 'sign-up-tourist', component: SignUpTouristComponent },
+  { path: 'sign-up-agency', component: SignUpAgencyComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'home', component: HomeComponent },
   { path: 'account', component: AccountComponent },
   { path: 'account-agency', component: AccountAgencyComponent },
   { path: 'account-change-password', component: ChangePasswordComponent },
   { path: 'account-upgrade-plan', component: UpgradePlansComponent },
   { path: 'account-payment-details', component: PaymentDetailsComponent },
-  { path: 'request', component: RequestAgencyComponent},
+  { path: 'request', component: RequestAgencyComponent },
   { path: 'add-service', component: AddServiceComponent },
   { path: 'add-service/:id', component: AddServiceComponent },
   { path: 'service-detail/:id', component: ServiceDetailsComponent },
   { path: 'search/:text', component: SearchComponent },
   { path: '**', component: PageNotFoundComponent },
-
 ];
 
 @NgModule({
--- go2climb/src/app/components/account/account.component.ts
+++ go2climb/src/app/components/account/account.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Tourist } from 'src/app/models/tourist';
+import { TouristService } from 'src/app/services/tourist.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  userId = sessionStorage.getItem('userid') as string;
 
+  currentTourist: Tourist | undefined = undefined;
+
+  touristData = this.formBuilder.group({
+    name: '',
+    email: '',
+    phone: '',
+    location: '',
+  });
+
+  imgUrl = '../../../assets/user-icon.webp';
+
+  constructor(
+    private touristService: TouristService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.touristService.getById(this.userId).subscribe((tourist) => {
+      this.currentTourist = tourist;
+
+      this.touristData.setValue({
+        name: tourist.name,
+        email: tourist.email,
+        phone: String(tourist.phoneNumber),
+        location: tourist.address,
+      });
+
+      this.imgUrl = tourist.photo;
+    });
+  }
+
+  updateTourist(): void {
+    const { name, email, location, phone } = this.touristData.value;
+
+    if (!this.currentTourist) return;
+
+    this.touristService
+      .update({
+        ...this.currentTourist,
+        name: String(name),
+        email: String(email),
+        phoneNumber: Number(phone),
+        address: String(location),
+      })
+      .subscribe();
+  }
 }
